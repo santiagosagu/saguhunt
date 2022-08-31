31413adc804ebebc700206298f05f56a9d4dc99d
Danielgiraldo <daniel.giraldo@atlantisoft.us>
2022-08-31 12:20:24 -0500

    new deployment

---
+++ firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDKaM8utireURezVoKsU0Qd1rnhPGk4KnE",
+  authDomain: "sagu-hunt.firebaseapp.com",
+  databaseURL: "https://sagu-hunt.firebaseio.com",
+  projectId: "sagu-hunt",
+  storageBucket: "sagu-hunt.appspot.com",
+  messagingSenderId: "924737193371",
+  appId: "1:924737193371:web:190372632b4b9a0bcf4d3e",
+  measurementId: "G-VBDYGHKVF4",
+};
+
+export default firebaseConfig;
